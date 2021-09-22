91b31640ed2d4664dcc59e1fdedb32a9135fd361
Maomurillo4 <maomurillo453@gmail.com>
2021-09-21 19:32:09 -0500

    Primer Commit

---
+++ ayuda.js
@@ -0,0 +1,127 @@
+//los 32 departamentos de colombia jaja ayuda que onda wtf
+
+const nombreDepto = () =>{
+    let departamento = prompt ("Ingresa departamento sin tildes porfa :)").toLowerCase();
+
+    switch(departamento) {
+        case "tolima":
+            alert("La capital de " + departamento+ " es Ibagué")
+            break;
+    
+            case "amazonas":
+            alert("La capital de " + departamento+ " es Leticia")
+            break;
+    
+            case "antioquia":
+            alert("La capital de " + departamento+ " es Medellín")
+            break;
+    
+            case "arauca":
+            alert("La capital de " + departamento+ " es Arauca")
+            break;
+    
+            case "atlantico":
+            alert("La capital de " + departamento+ " es Barranquilla")
+            break;
+    
+            case "bolivar":
+            alert("La capital de " + departamento+ " es Cartagena de Indias")
+            break;
+    
+            case "boyaca":
+            alert("La capital de " + departamento+ " es Tunja")
+            break;
+    
+            case "caldas":
+            alert("La capital de " + departamento+ " es Manizales")
+            break;
+    
+            case "caqueta":
+            alert("La capital de " + departamento+ " es Florencia")
+            break;
+    
+            case "casanare":
+            alert("La capital de " + departamento+ " es Yopal")
+            break;
+    
+            case "cauca":
+            alert("La capital de " + departamento+ " es Popayán")
+            break;
+    
+            case "cesar":
+            alert("La capital de " + departamento+ " es Valledupar")
+            break;
+    
+            case "choco":
+            alert("La capital de " + departamento+ " es Quibdó")
+            break;
+            
+            case "cordoba":
+            alert("La capital de " + departamento+ " es Monteria")
+            break;
+    
+            case "cundinamarca":
+            alert("La capital de " + departamento+ " es Bogotá")
+            break;
+    
+            case "guainia":
+            alert("La capital de " + departamento+ " es Inírida")
+            break;
+    
+            case "guaviare":
+            alert("La capital de " + departamento+ " es San José del Guaviare")
+            break;
+    
+            case "huila":
+            alert("La capital de " + departamento+ " es Neiva")
+            break;
+    
+            case "la guajira":
+            alert("La capital de " + departamento+ " es Rioacha")
+            break;
+
+            case "magdalena":
+            alert("La capital de " + departamento+ " es Santa Marta")
+            break;
+
+            case "meta":
+            alert("La capital de " + departamento+ " es Villavicencio")
+            break;
+
+            case "nariño":
+            alert("La capital de " + departamento+ " es Pasto")
+            break;
+
+            case "norte de santander":
+            alert("La capital de " + departamento+ " es Cúcuta")
+            break;
+
+            case "putumayo":
+            alert("La capital de " + departamento+ " es Mocoa")
+            break;
+
+            case "quindio":
+            alert("La capital de " + departamento+ " es Armenia")
+            break;
+
+            case "risaralda":
+            alert("La capital de " + departamento+ " es Pereira")
+            break;
+
+            case "valle del cauca":
+            alert("La capital de " + departamento+ " es Cali")
+            break;
+
+            case "vaupes":
+            alert("La capital de " + departamento+ " es Mitú")
+            break;
+
+            case "vichada":
+            alert("La capital de " + departamento+ " es Puerto Carreño")
+            break;
+        
+        default:
+            alert("Que trollo me saliste jaja")
+            break;
+    }
+}
